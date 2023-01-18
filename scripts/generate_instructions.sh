97e0a8d907a9b4c4a1ba8e85d6423c7984b61f56
zhixhan <18811378759@163.com>
2023-01-18 15:20:08 +0800

    init

---
+++ scripts/generate_instructions.sh
@@ -0,0 +1,12 @@
+KEY=$1
+BASE=$2
+
+batch_dir=data/gpt3_generations/
+
+python self_instruct/bootstrap_instructions.py \
+    --batch_dir ${batch_dir} \
+    --num_instructions_to_generate 50000 \
+    --seed_tasks_path data/seed_tasks.jsonl \
+    --engine "text-davinci-003" \
+    --api_key $KEY \
+    --api_base $BASE
