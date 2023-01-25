618fa9e13393051dbc49db1016328624bb148fc7
zhixhan <38372560+zhixhan@users.noreply.github.com>
2023-01-25 15:13:41 +0800

    Update generate_instructions.sh

--- scripts/generate_instructions.sh
+++ scripts/generate_instructions.sh
@@ -1,9 +1,13 @@
 
 batch_dir=data/gpt3_generations_original_seed/
 
-python self_instruct/bootstrap_instructions.py \
-    --batch_dir ${batch_dir} \
-    --num_instructions_to_generate 40000 \
-    --seed_tasks_path data/seed_tasks.jsonl \
-    --engine "text-davinci-003" \
-    --request_batch_size 20
+for i in {1..500}
+do
+    export AKEY=$(az account get-access-token --resource https://ml.azure.com -o tsv | cut -d $'\t' -f1)
+    python self_instruct/bootstrap_instructions.py \
+        --batch_dir ${batch_dir} \
+        --num_instructions_to_generate 40000 \
+        --seed_tasks_path data/seed_tasks.jsonl \
+        --engine "text-davinci-003" \
+        --request_batch_size 20
+done
