13ea2715bd08992d2f201c8020f0e40f95dcf658
zhixhan <38372560+zhixhan@users.noreply.github.com>
2023-01-25 14:52:43 +0800

    Update generate_instructions.sh

--- scripts/generate_instructions.sh
+++ scripts/generate_instructions.sh
@@ -3,7 +3,7 @@ batch_dir=data/gpt3_generations_original_seed/
 
 for i in {1..500}
 do
-    export AKEY=$(az account get-access-token --resource https://ml.azure.com -o tsv | cut -d $'\t' -f1)
+    #export AKEY=$(az account get-access-token --resource https://ml.azure.com -o tsv | cut -d $'\t' -f1)
     python self_instruct/bootstrap_instructions.py \
         --batch_dir ${batch_dir} \
         --num_instructions_to_generate 40000 \
